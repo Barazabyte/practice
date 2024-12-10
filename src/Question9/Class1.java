2e4c614dab9ece25f09c05e90ae0e44bd9dc8aef
Baraza <barazars45@gmail.com>
2024-12-10 11:07:58 +0300

    Add files via upload

---
+++ src/Question9/Class1.java
@@ -0,0 +1,12 @@
+package Question9;
+
+public class Class1 {
+    final int age;
+
+    public Class1(int age) {
+        this.age = age;
+    }
+    public double getAge() {
+        return this.age;
+    }
+}
---
+++ src/Question9/Main.java
@@ -0,0 +1,8 @@
+package Question9;
+
+public class Main {
+    public static void main(String[] args) {
+        Class1 fill = new Class1(50);
+        System.out.println(" Age: " + fill.getAge());
+    }
+}
